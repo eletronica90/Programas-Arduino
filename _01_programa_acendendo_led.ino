788f3ff7c0e771ac2f15e51c329ee64063d489a1
Eletrônica de Garagem <eletronicadegaragem@outlook.com>
2018-05-27 18:08:42 -0300

    Add files via upload

---
+++ _01_programa_acendendo_led.ino
@@ -0,0 +1,18 @@
+// TÍTULO: 01 - Programa Para Acender um LED
+// AUTOR: Ângelo Meneguini
+// SITE: https://eletronicagaragem.blogspot.com.br/
+
+int ledPin = 10;                  // pino onde o led será conectado
+
+void setup() 
+{   
+    pinMode(ledPin, OUTPUT);      // configura o pino 10 como saída
+}
+
+void loop()
+{    
+    digitalWrite(ledPin, HIGH);   // aciona a saída, pino 10 com 5V
+    delay(1000);                  // espera por 1 segundo
+    digitalWrite(ledPin, LOW);    // desativa a saída, pino 10 com 0V
+    delay(1000);                  // espera por 1 segundo
+}
